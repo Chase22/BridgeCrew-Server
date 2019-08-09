70c95f2f320f2d5e57efc7241bfe6e90a2a2f2e3
chase <chase@mailbox.org>
2019-08-09 17:53:53 +0200

    Extract ShipOperations Interface

--- src/main/kotlin/io/github/chase22/bridgecrew/server/ship/ShipController.kt
+++ src/main/kotlin/io/github/chase22/bridgecrew/server/ship/ShipController.kt
@@ -1,30 +1,35 @@
 package io.github.chase22.bridgecrew.server.ship
 
+import io.github.chase22.bridgecrew.server.subsystem.Subsystem
+import io.github.chase22.bridgecrew.server.subsystem.SubsystemType
 import io.github.chase22.bridgecrew.server.websocket.WebSocketServer
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.MutableHttpResponse
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.*
+import io.reactivex.Single
 import javax.inject.Inject
 
 @Controller("/ship")
 class ShipController @Inject constructor(
         private val webSocketServer: WebSocketServer
-) {
+) : ShipOperations {
+
+    override fun getSubsystems(id: String, type: SubsystemType?): Single<MutableHttpResponse<List<Subsystem>>> {
+        return Single.just(HttpResponse.ok(emptyList()))
+    }
+
     private var ship: Ship = Ship(
             100, 150, 300, 100, 100, true, emptyList()
     )
 
     @Get
-    fun getShip(): MutableHttpResponse<Ship> = HttpResponse.ok(ship)
+    override fun getShip(@PathVariable id: String): Single<MutableHttpResponse<Ship>> = Single.just(HttpResponse.ok(ship))
 
     @Post
-    fun postShip(@Body ship: Ship): HttpStatus {
+    override fun postShip(@Body ship: Ship): Single<HttpStatus> {
         this.ship = ship
         webSocketServer.broadcast(ship)
-        return HttpStatus.CREATED
+        return Single.just(HttpStatus.CREATED)
     }
 }
---
+++ src/main/kotlin/io/github/chase22/bridgecrew/server/ship/ShipOperations.kt
@@ -0,0 +1,22 @@
+package io.github.chase22.bridgecrew.server.ship
+
+import io.github.chase22.bridgecrew.server.subsystem.Subsystem
+import io.github.chase22.bridgecrew.server.subsystem.SubsystemType
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.MutableHttpResponse
+import io.micronaut.http.annotation.*
+import io.reactivex.Single
+
+interface ShipOperations {
+
+    @Get("/{id}")
+    fun getShip(@PathVariable id: String): Single<MutableHttpResponse<Ship>>
+
+    @Get("/{id}/subsystems")
+    fun getSubsystems(@PathVariable id: String, @QueryValue type: SubsystemType?): Single<MutableHttpResponse<List<Subsystem>>>
+
+    @Post
+    fun postShip(@Body ship: Ship): Single<HttpStatus>
+
+
+}
